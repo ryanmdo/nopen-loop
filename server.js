52d792134b59587a30279f1042c5d660eca0f6f6
ryanmdo <rdo9842@gmail.com>
2018-09-25 02:58:27 -0700

    connection fixed for mongodb 3.1.0 connect function, some deprecated warning

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const routes = require('./routes');
 
+
+
 const mongoose = require('mongoose');
 
 
@@ -15,9 +17,10 @@ const app = express();
 app.use(routes);
 
 
+//MongoClient.connect("mongodb://localhost:27017/YourDB", { useNewUrlParser: true })
+//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nopen_loop_dev_db");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nopen_loop_dev_db");
-
+mongoose.connect("mongodb://localhost:27017/nopen_loop_dev_db", { useNewUrlParser: true })
 
 
 const PORT = process.env.PORT || 5000;
